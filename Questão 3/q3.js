a7f95ccf11bc05afe55b9de47b28f9d023fd9aca
polvinh0 <ieiel_4@hotmail.com>
2022-09-18 20:24:38 -0300

    correção

--- Questão 3/q3.js
+++ Questão 3/q3.js
@@ -1,9 +1,9 @@
-Alert("Questão 3")
+alert("Questão 3")
 
 let raio = Number(prompt(" Por favor, Digite o valor do raio"))
 const pi = (3.1415)
 let diametro = (raio ** 2)    
 let area = (2*(pi*raio))
 let perimetro = (pi*(diametro))
-alert("O valor da aréa é " + area)
+alert("O valor da aréa é " + Math.round(area))
 alert("O valor do perímetro é " + perimetro) 
